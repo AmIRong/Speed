d5b97b4a5730c4102b78f1f9e9e2864d17e31657
woke <woke@172.16.39.175>
2017-02-14 15:52:52 +0800

    路由入口

---
+++ data/sysdata/cache_domain.php
@@ -0,0 +1,28 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: eee948c5ce0e202f44fc3bb20e8db1d7
+
+$domain = array (
+  'defaultindex' => 'forum.php',
+  'holddomain' => 'www|*blog*|*space*|*bbs*',
+  'list' => 
+  array (
+  ),
+  'app' => 
+  array (
+    'portal' => '',
+    'forum' => '',
+    'group' => '',
+    'home' => '',
+    'default' => '',
+  ),
+  'root' => 
+  array (
+    'home' => '',
+    'group' => '',
+    'forum' => '',
+    'topic' => '',
+    'channel' => '',
+  ),
+);
+?>
